16c6dd2743e0432e3b0ea4a47fa78ea05f930100
Rajdeep <raorajdeep30@gmail.com>
2022-04-18 13:32:38 +0530

    SecondCommit

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import SignUp from '../Orchid/Project/SignUp'
-//import Demo from '../Sunflower/Project/Demo'
-import SignIn from '../Orchid/Project/SignIn'
+import SignUp from './Project/SignUp';
+import SignIn from './Project/SignIn';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
-//import SplashScreen from 'react-native-splash-screen'
-//import { useEffect } from 'react/cjs/react.production.min';
-//import React, { useEffect, ReactElement } from 'react';
-//import Router from 'next/router';
 
 const App = () => {
 
